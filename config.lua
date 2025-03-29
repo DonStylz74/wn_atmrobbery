631f52bbfc1fec4c9312a9d8dd4063ec662712d0
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2025-03-30 00:44:21 +1100

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,7 +10,7 @@ Config.Target = "ox_target" -- ox_target, qb-target
 Config.Notify = "ox_lib" -- ox_lib, qbcore or ESX
 Config.Dispatch = "cd_dispatch" -- cd_dispatch, linden_outlawalert, ps-disptach, core-dispatch, qs-dispatch
 Config.SearchDuration = 6000 -- How long will the player search the pleace IN MS
-Config.HackingDevice = "hacking_device" -- Name for your hacking item
+Config.HackingDevice = "hacking_device_atm" -- Name for your hacking item
 Config.HackingMinigames = { -- Types of hacking games in the hacking menu
     "numeric",
     "alphabet",
@@ -20,7 +20,7 @@ Config.HackingMinigames = { -- Types of hacking games in the hacking menu
     "runes"
 }
 
-Config.Props = { 'prop_atm_01', 'prop_atm_02', 'prop_fleeca_atm', 'prop_atm_03' } -- Prop names for ATMs !
-Config.Reward = math.random(100, 500) -- Cash Reward per bill.
+Config.Props = { 'prop_atm_01', 'prop_atm_02', 'prop_fleeca_atm', 'prop_atm_03', 'fm_fleeca_atm', 'fm_ltd_atm_01' } -- Prop names for ATMs !
+Config.Reward = math.random(550, 2500) -- Cash Reward per bill.
 Config.Cooldown = 10 -- Cooldwon in minutes.
-Config.CopsNeeded = 1 -- Cops needed to start the robbery.
+Config.CopsNeeded = 0 -- Cops needed to start the robbery.
